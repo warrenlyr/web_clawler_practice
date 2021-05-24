86ba96f677fb0fa3cd516f9901084eb17c85423c
Warren Liu <warrenlyr@gmail.com>
2021-05-23 20:51:41 -0700

    Update file_merge.py

--- src/file_merge.py
+++ src/file_merge.py
@@ -7,51 +7,60 @@ import os
 from tqdm import tqdm
 
 
-FILE_PATH = r'./data/'
+OUT_PATH = r'./data/merged_file/'
+FILE_PATH = r'./data/all_years/'
 FILE_NAMES = os.listdir(FILE_PATH)
 OUT_FILE = r'merged_file.csv'
 
 # merge files into one file
 def merge_file():
-    out_file = open(FILE_PATH + OUT_FILE, 'w')
+    if not os.path.exists(OUT_PATH):
+        os.mkdir(OUT_PATH)
+    out_file = open(OUT_PATH + OUT_FILE, 'w')
 
     for file in FILE_NAMES:
         this_file_path = FILE_PATH + file
-        print(r'Now writting file {} to {}'.format(file, OUT_FILE))
+        #print(r'Now writting file {} to {}'.format(file, OUT_FILE))
         for line in open(this_file_path):
             out_file.writelines(line)
 
-    print('Done')
+    print('[Done merging file]')
     out_file.close()
 
 
 # get rid of the date in the file
 def delete_date():
-    in_file = FILE_PATH + OUT_FILE
-    out_file = FILE_PATH + 'merged_file_without_date.cvs'
+    in_file = OUT_PATH + OUT_FILE
+    out_file = OUT_PATH + 'merged_file_without_date.csv'
     with open(in_file, 'r') as fin, open(out_file, 'w') as fout:
         for line in fin:
             l = line[10:]
             fout.writelines(l)
-    print('Input file lines: {} Output file lines: {}'.format(len(open(in_file).readlines()),(len(open(out_file).readlines()))))
+    print('delete_date: Input file lines: {} Output file lines: {}'.format(len(open(in_file).readlines()),(len(open(out_file).readlines()))))
     fin.close()
     fout.close()
+    print('[Done deleting date]')
 
 
 # get rid of the year in the file
 def delete_year():
-    in_file = FILE_PATH + OUT_FILE
-    out_file = FILE_PATH + 'merged_file_without_year.cvs'
+    in_file = OUT_PATH + OUT_FILE
+    out_file = OUT_PATH + 'merged_file_without_year.csv'
     with open(in_file, 'r') as fin, open(out_file, 'w') as fout:
         for line in fin:
             l = line[5:]
             fout.writelines(l)
-    print('Input file lines: {} Output file lines: {}'.format(len(open(in_file).readlines()),(len(open(out_file).readlines()))))
+    print('delete_year: Input file lines: {} Output file lines: {}'.format(len(open(in_file).readlines()),(len(open(out_file).readlines()))))
     fin.close()
     fout.close()
+    print('[Done deleting year]')
 
 
-if __name__ == '__main__':
+
+def main():
     merge_file()
     delete_date()
     delete_year()
+
+if __name__ == '__main__':
+    main()
